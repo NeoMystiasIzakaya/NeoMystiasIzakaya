86f8b678956dea9a5a51e4e81c0dee602b653ac2
Kaguya154 <kaguya154@outlook.com>
2026-05-06 21:29:09 +0800

    可以加载，赢

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/event/ReloadEventHandler.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/event/ReloadEventHandler.java
@@ -3,13 +3,12 @@ package icu.gensoukyo.neo_mystias_izakaya.common.event;
 import com.mojang.logging.LogUtils;
 import icu.gensoukyo.neo_mystias_izakaya.NeoMystiasIzakaya;
 import icu.gensoukyo.neo_mystias_izakaya.content.tag.TagItemListReloadListener;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 import org.slf4j.Logger;
 
-@EventBusSubscriber(modid = NeoMystiasIzakaya.MODID,value = Dist.DEDICATED_SERVER)
+@EventBusSubscriber(modid = NeoMystiasIzakaya.MODID)
 public class ReloadEventHandler {
     private static final Logger LOGGER = LogUtils.getLogger();
 
