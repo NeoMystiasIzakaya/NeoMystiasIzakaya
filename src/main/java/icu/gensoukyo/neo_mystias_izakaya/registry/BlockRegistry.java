7e60cc142cbefb8c3c11dd4cf941d6031bffa0d3
Kaguya154 <kaguya154@outlook.com>
2026-05-06 19:33:38 +0800

    把datagen拆出来力

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/registry/BlockRegistry.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/registry/BlockRegistry.java
@@ -8,5 +8,5 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class BlockRegistry {
     //BLOCKS is a DeferredRegister.Blocks
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NeoMystiasIzakaya.MODID);
-    public static final DeferredBlock<Block> MY_BLOCK = BLOCKS.register("my_block", registryName -> new Block());
+//    public static final DeferredBlock<Block> MY_BLOCK = BLOCKS.register("my_block", registryName -> new Block());
 }
