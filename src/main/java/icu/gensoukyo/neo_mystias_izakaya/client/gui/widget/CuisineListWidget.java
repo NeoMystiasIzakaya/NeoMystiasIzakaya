0b5d238fd38224f845002d12ca8505c7590999b5
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-05-19 00:48:02 +0800

    我已经完全学会ObjectSelectionList了

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/client/gui/widget/CuisineListWidget.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/client/gui/widget/CuisineListWidget.java
@@ -1,34 +1,62 @@
 package icu.gensoukyo.neo_mystias_izakaya.client.gui.widget;
 
 import icu.gensoukyo.neo_mystias_izakaya.client.gui.RecipeScreen;
+import icu.gensoukyo.neo_mystias_izakaya.content.recipe.NMIRecipeHolder;
+import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 
 public class CuisineListWidget extends ObjectSelectionList<CuisineListWidget.CuisineEntry>{
     private final RecipeScreen parent;
+    private final int listWidth;
     public CuisineListWidget(RecipeScreen parent,Minecraft minecraft, int width, int height, int y, int itemHeight) {
         super(minecraft, width, height, y, itemHeight);
         this.parent = parent;
+        this.listWidth = width;
+    }
+
+    @Override
+    public int getRowWidth() {
+        return this.listWidth - 12;
+    }
+
+    @Override
+    protected int scrollBarX() {
+        return (super.scrollBarX() - 8);
+    }
+
+    @Override
+    protected void extractListBackground(GuiGraphicsExtractor graphics) {
+
+    }
+
+    @Override
+    protected void extractListSeparators(GuiGraphicsExtractor graphics) {
+
     }
 
     public void refreshList() {
         this.clearEntries();
+        parent.buildImageList(this::addEntry, cuisine -> new CuisineEntry(cuisine, this.parent));
     }
 
-
-    public class CuisineEntry extends ObjectSelectionList.Entry<CuisineListWidget.CuisineEntry> {
-        private final ItemStack cuisine;
-        public CuisineEntry(ItemStack cuisine) {
+    public static class CuisineEntry extends ObjectSelectionList.Entry<CuisineListWidget.CuisineEntry> {
+        @Getter
+        private final NMIRecipeHolder cuisine;
+        private final RecipeScreen parent;
+        public CuisineEntry(NMIRecipeHolder cuisine, RecipeScreen parent) {
             this.cuisine = cuisine;
+            this.parent = parent;
         }
 
         @Override
         public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+            this.parent.setSelected(this);
             return super.mouseClicked(event, doubleClick);
         }
 
@@ -38,8 +66,11 @@ public class CuisineListWidget extends ObjectSelectionList<CuisineListWidget.Cui
         }
 
         @Override
-        public void extractContent(GuiGraphicsExtractor guiGraphicsExtractor, int i, int i1, boolean b, float v) {
+        public void extractContent(GuiGraphicsExtractor graphics, int mouseX, int mouseY, boolean hovered, float a) {
             Font font = Minecraft.getInstance().font;
+            Item value = cuisine.recipe().output().item().value();
+            graphics.text(font, Component.translatable(value.getDescriptionId()), getX() + 18, getY() + 4, 0xFFFFFFFF);
+            graphics.item(value.getDefaultInstance(), getX(), getY());
         }
     }
 }
