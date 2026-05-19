994e1e6f7e886630f9eb7565ac83849eea360fc6
Kaguya154 <kaguya154@outlook.com>
2026-05-19 20:21:55 +0800

    一个方法

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/util/NMICommonItemTagUtil.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/util/NMICommonItemTagUtil.java
@@ -5,15 +5,26 @@
 
 package icu.gensoukyo.neo_mystias_izakaya.common.util;
 
+import icu.gensoukyo.neo_mystias_izakaya.content.tag.TagItemListHolder;
 import net.minecraft.resources.Identifier;
+
+import java.util.Map;
 
 public class NMICommonItemTagUtil {
 
-    public static boolean isBeveragesTag(Identifier tag){
+    public static boolean isBeveragesTag(Identifier tag) {
         return tag.getPath().startsWith("beverages/");
     }
 
-    public static boolean isFoodsTag(Identifier tag){
+    public static boolean isCuisinesTag(Identifier tag) {
         return tag.getPath().startsWith("cuisines/");
+    }
+
+    public static Map<Identifier, TagItemListHolder> getBeveragesTagMap(Map<Identifier, TagItemListHolder> original) {
+        return original.entrySet().stream().filter(entry -> isBeveragesTag(entry.getKey())).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public static Map<Identifier, TagItemListHolder> getCuisinesTagMap(Map<Identifier, TagItemListHolder> original) {
+        return original.entrySet().stream().filter(entry -> isCuisinesTag(entry.getKey())).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
