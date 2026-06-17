27f042a1d94961d0032515a0672b4b853ee26813
Kaguya154 <kaguya154@outlook.com>
2026-06-17 17:29:15 +0800

    修qwq

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/client/event/RegisterArmorRender.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/client/event/RegisterArmorRender.java
@@ -1,5 +1,6 @@
 package icu.gensoukyo.neo_mystias_izakaya.client.event;
 
+import icu.gensoukyo.neo_mystias_izakaya.NeoMystiasIzakaya;
 import icu.gensoukyo.neo_mystias_izakaya.client.render.NMIArmorRender;
 import icu.gensoukyo.neo_mystias_izakaya.registry.item.NMIMainItems;
 import net.neoforged.api.distmarker.Dist;
@@ -7,7 +8,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
-@EventBusSubscriber(value = Dist.CLIENT)
+@EventBusSubscriber(modid = NeoMystiasIzakaya.MODID, value = Dist.CLIENT)
 public class RegisterArmorRender {
     @SubscribeEvent
     public static void registerRender(RegisterClientExtensionsEvent event) {
